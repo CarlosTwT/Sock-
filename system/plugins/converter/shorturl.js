82a022ba5b879b89ec9b0e5719663fb7334acdc4
I'am Carlos <103902036+CarlosTwT@users.noreply.github.com>
2025-06-19 15:45:05 -0500

    Update shorturl.js

--- system/plugins/converter/shorturl.js
+++ system/plugins/converter/shorturl.js
@@ -17,9 +17,21 @@ module.exports = {
     try {
       await m.reply("> Acortando URL...");
       const { data: tinyUrlShortUrl } = await axios.get(`https://tinyurl.com/api-create.php?url=${text}`);
-      await sock.sendMessage(m.cht, {
-        text: `*– 乂 URL Acortada 乂 –*\n\n> *- TinyURL:* ${tinyUrlShortUrl}`
-      }, { quoted: m });
+
+      const interactiveMessage = {
+        text: `*– 乂 URL Acortada 乂 –*\n\n`,
+        footer: "© Sock Bot",
+        interactiveButtons: [{
+            name: "cta_copy",
+            buttonParamsJson: JSON.stringify({
+                display_text: "Copiar Enlace",
+                copy_code: tinyUrlShortUrl
+            })
+        }]
+      };
+
+      await sock.sendMessage(m.cht, interactiveMessage, { quoted: m });
+
     } catch (error) {
       console.error("Error al acortar URL:", error);
       m.reply("> No se pudo acortar la URL. Intenta nuevamente.");
